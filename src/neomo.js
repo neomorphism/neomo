8f124e88f3ec6d629a4a885a170635ba2d7b922e
dnflekals <68384667+dnflekals@users.noreply.github.com>
2021-03-27 14:59:44 +0900

    feat: add file function

--- src/neomo.js
+++ src/neomo.js
@@ -6,6 +6,22 @@ alt.addEventListener("click", function () {
 });
 /* Alert function end */
 
+/* File function start */
+function filename() {
+  $(document).ready(function () {
+    var fileTarget = $(".file .file-hidden");
+    fileTarget.on("change", function () {
+      if (window.FileReader) {
+        var filename = $(this)[0].files[0].name;
+      } else {
+        var filename = $(this).val().split("/").pop().split("\\").pop();
+      }
+      $(this).siblings(".file-name").val(filename);
+    });
+  });
+}
+/* File function end */
+
 /* Modal function start */
 var modal = document.getElementById("Modal");
 var btn = document.getElementById("ModalBtn");
