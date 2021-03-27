8f01e1abb0387d0b66689687232c9f4476a71e9d
leaping <leaping96@ajou.ac.kr>
2021-03-27 21:02:48 +0900

    fix: move function toast.js into neomo.js

--- src/neomo.js
+++ src/neomo.js
@@ -55,3 +55,10 @@ function TabToggle() {
   });
 }
 /* Tab function end */
+
+
+/* Toast function start */
+function toast_button(obj) {
+  obj.closest(".toast").classList.remove("show");
+}
+/* Toast function end */
